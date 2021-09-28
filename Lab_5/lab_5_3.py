95848b4ef27f064f81c33f0ec4632fae78da2864
Luis <luiswoohoo@gmail.com>
2021-09-28 14:07:27 -0500

    lab 5.3.2 complete

--- Lab_5/lab_5_3.py
+++ Lab_5/lab_5_3.py
@@ -4,7 +4,7 @@ random.seed(5)
 keep_bidding = '-'
 next_bid = 0
 
-while ''' Your solution goes here ''':
+while keep_bidding != 'n':
    next_bid = next_bid + random.randint(1, 10)
    print('I\'ll bid ${}!'.format(next_bid))
    print('Continue bidding?', end=' ')
