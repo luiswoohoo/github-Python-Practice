70860b039939addd4008e767e733cf8309e3aea8
Luis <luiswoohoo@gmail.com>
2021-09-28 14:16:01 -0500

    Lab 5.3.3 complete

--- Lab_5/lab_5_3.py
+++ Lab_5/lab_5_3.py
@@ -1,11 +1,8 @@
-import random
-random.seed(5)
+num_insects = int(input()) # Must be >= 1
 
-keep_bidding = '-'
-next_bid = 0
+if num_insects >= 1:
+    while num_insects <= 100:
+        print(num_insects, end=" ")
+        num_insects *= 2
 
-while keep_bidding != 'n':
-   next_bid = next_bid + random.randint(1, 10)
-   print('I\'ll bid ${}!'.format(next_bid))
-   print('Continue bidding?', end=' ')
-   keep_bidding = input()
+
