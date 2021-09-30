0872f0cf96b94df3dd3ae76c7a595d7358f6c7e9
Luis <luiswoohoo@gmail.com>
2021-09-30 17:50:02 -0500

    Lab 5.15 complete

--- Lab_5/lab_5_15.py
+++ Lab_5/lab_5_15.py
@@ -1,3 +1,24 @@
 word = input()
 password = ''
 
+password_modifier = {
+    'i': '1',
+    'a': '@',
+    'm': 'M',
+    'B': '8',
+    's': '$'
+}
+
+for (index, char_word) in enumerate(word):
+
+    if char_word in password_modifier:
+        for char_mod in password_modifier:
+            if char_word == char_mod:
+                password += password_modifier[char_mod]
+
+    else:
+        password += char_word
+
+password += '!'
+
+print(password)
