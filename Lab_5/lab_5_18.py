277739888d1e930cab703b616daac8a11f281759
Luis <luiswoohoo@gmail.com>
2021-09-30 19:16:25 -0500

    Lab 5.18

---
+++ Lab_5/lab_5_18.py
@@ -0,0 +1,11 @@
+''' Read in first equation, ax + by = c '''
+a = int(input())
+b = int(input())
+c = int(input())
+
+''' Read in second equation, dx + ey = f '''
+d = int(input())
+e = int(input())
+f = int(input())
+
+''' Type your code here. '''
