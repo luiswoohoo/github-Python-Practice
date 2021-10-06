fc479344adb4d808fd8b771c4efd098c3fef140c
Luis <luiswoohoo@gmail.com>
2021-10-06 17:31:52 -0500

    Lab 6.13.2 complete

--- Lab_6/lab_6_13_2.py
+++ Lab_6/lab_6_13_2.py
@@ -1,4 +1,5 @@
-
+def number_of_pennies(dollars, pennies=0):
+    return'{}{:02d}'.format(dollars, pennies)
 
 print(number_of_pennies(int(input()), int(input()))) # Both dollars and pennies
 print(number_of_pennies(int(input())))               # Dollars only
