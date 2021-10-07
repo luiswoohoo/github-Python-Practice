1de0e2ce7844a70ed8db4d2afb13726a85eddfdc
Luis <luiswoohoo@gmail.com>
2021-10-07 16:05:06 -0500

    Lab 6.22
    Correctly swaps two integers.

--- Lab_6/lab_6_22.py
+++ Lab_6/lab_6_22.py
@@ -1,1 +1,10 @@
-#Test
+def swap_values(user_val1, user_val2):
+    return user_val2, user_val1
+
+
+value1 = int(input())
+value2 = int(input())
+
+swapped_values = swap_values(value1, value2)
+
+print('{} {}'.format(swapped_values[0], swapped_values[1]))
