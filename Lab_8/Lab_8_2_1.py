2c11416260538c6d4cd8f4536e4ca42965b37313
Luis <luiswoohoo@gmail.com>
2021-10-19 12:42:41 -0500

    Lab 8.2.1
    Complete

--- Lab_8/Lab_8_2_1.py
+++ Lab_8/Lab_8_2_1.py
@@ -7,29 +7,50 @@ menu = ('(1) Reserve place in line.\n'  # Add rider to line
         '(2) Reserve place in VIP line.\n'  # Add VIP
         '(3) Dispatch riders.\n'  # Dispatch next ride car
         '(4) Print riders.\n'
-        '(5) Exit.\n\n')
+        '(5) What\'s my spot in line?\n'
+        '(6) Remove me.\n'
+        '(7) Exit.\n\n')
 
 user_input = input(menu).strip().lower()
 
-while user_input != '5':
+while user_input != '7':
     if user_input == '1':  # Add rider
         name = input('Enter name:').strip().lower()
         print(name)
         line.append(name)
 
     elif user_input == '2':  # Add VIP
-        print('FIXME: Add new VIP')
         # Add new rider behind last VIP in line
-        # Hint: Insert the VIP into the line at position num_vips.
-        #Don't forget to increment num_vips.
+        name = input('Enter name:').strip().lower()
+        print(name)
+        line.insert(num_vips, name)
+        num_vips += 1
 
     elif user_input == '3':  # Dispatch ride
-        print('FIXME: Remove riders from the front of the line.')
         # Remove last riders_per_ride from front of line.
         # Don't forget to decrease num_vips, if necessary.
+        del line[0:3]
+        if num_vips > 3:
+            num_vips -= 3
+        else:
+            num_vips = 0
 
     elif user_input == '4':  # Print riders waiting in line
         print('{} person(s) waiting:'.format(len(line)), line)
+
+    elif user_input == '5':  # Enter a name to find the current position in line
+        name = input('Enter name:').strip().lower()
+        if name in line:
+            print('You are number:' + (str(line.index(name) + 1)))
+        else:
+            print('That rider isn\'t in the line.')
+
+    elif user_input == '6':  # Enter a name to remove the rider from the line
+        name = input('Enter name:').strip().lower()
+        if name in line:
+            line.remove(name)
+        else:
+            print('That rider isn\'t in the line.')
 
     else:
         print('Unknown menu option')
