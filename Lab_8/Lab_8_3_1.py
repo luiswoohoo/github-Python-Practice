165783ac6ccaf41e4f6f70f2ac75158229b77e27
Luis <luiswoohoo@gmail.com>
2021-10-20 17:40:58 -0500

    Lab 8.3.1
    Complete

--- Lab_8/Lab_8_3_1.py
+++ Lab_8/Lab_8_3_1.py
@@ -1,6 +1,10 @@
 num_guesses = int(input())
 user_guesses = []
 
-''' Your solution goes here '''
+num = 0
+while num < num_guesses:
+    guess = int(input())
+    user_guesses.append(guess)
+    num += 1
 
 print('user_guesses:', user_guesses)
