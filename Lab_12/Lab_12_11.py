927b2b9a74e0233b0e76b9945f88303095776e63
Luis <luiswoohoo@gmail.com>
2021-11-16 10:14:20 -0600

    Lab 12.11
    Complete! Don't even know how I got it to work, but it's pretty cool!

--- Lab_12/Lab_12_11.py
+++ Lab_12/Lab_12_11.py
@@ -24,10 +24,16 @@ class Flower(Plant):
 
 
 # TODO:  Define the print_list() function that prints a list of plant (or flower) objects
+def print_list(plant_list):
+    for plant in plant_list:
+        plant.print_info()
+        print()
+
 
 if __name__ == "__main__":
 
     # TODO: Declare a list called my_garden that can hold object of type plant
+    my_garden = []
 
     user_string = input()
 
@@ -36,6 +42,24 @@ if __name__ == "__main__":
         #       Split the user_string input into variables - plant_name, plant_cost, color_of_flowers, is_annual
         #       Store as a plant object or flower object
         #       Add to the list my_garden
+        input_data = user_string.split()
+
+        if input_data[0] == 'plant':
+            plant_name = input_data[1]
+            plant_cost = input_data[2]
+            my_plant = Plant(plant_name, plant_cost)
+            my_garden.append(my_plant)
+
+        if input_data[0] == 'flower':
+            plant_name = input_data[1]
+            plant_cost = input_data[2]
+            is_annual = input_data[3]
+            color_of_flowers = input_data[4]
+            my_flower = Flower(plant_name, plant_cost, is_annual, color_of_flowers)
+            my_garden.append(my_flower)
+
         user_string = input()
 
+
     # TODO: Call the print_list() function to print my_garden
+    print_list(my_garden)
